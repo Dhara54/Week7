7b7a53a96b85d64590f26a4a43e8f748db1985c4
dhara bharatbhai patel <dbp54@afsconnect2.njit.edu>
2017-11-05 22:12:11 -0500

    table

--- collection.php
+++ collection.php
@@ -1,59 +1,42 @@
-<?php
-include 'dbConn.php';
 
-class collection{
+ <?php
+ include 'dbConn.php';
 
-public function __construct(){
-  $class1=get_called_class();
-  $this->search(6,$class1);
-}	
-	
-public function search($count,$class1){
-    $db=dbConn::getConnection();
-    $sql = 'SELECT * FROM '.$class1.' where id< :count1';
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam(':count1', $count);
-    $stmt->execute();	
-    $rows=$stmt->rowCount();
-    echo 'Row count= '.$rows;
-    echo '<br>';
+ class collection{
 
-   $rowtotal=$stmt->fetchALL(PDO::FETCH_ASSOC);
->     echo "<table border=2>";
->     $this->displayHeader($class1);
->     $this->displayTable($rowtotal);
->     echo "</table>";
+ public function __construct(){
+   $class1=get_called_class();
+   $this->search(6,$class1);
+ }
+
+ 
+
+ public function displayHeader($class1){
+   $db1=dbConn::getConnection();
+   $sql1 = 'SHOW COLUMNS FROM '.$class1;
+   $stmt1 = $db1->prepare($sql1);
+   $stmt1->execute();
+   $headers=$stmt1->fetchAll(PDO::FETCH_COLUMN);
+
+   foreach($headers as $col){
+         echo "<td>$col</td>";
+         }
+     }
+
+ public function displayTable($e){
+
+     foreach( $e as $row) {
+       echo "<tr>";
+       foreach($row as $col){
+         echo "<td>$col</td>";
+         }
+         echo "<tr>";
+       }
+     }
+ }
+ class accounts extends collection{
 
 
 
-    
-    $rowtotal=$stmt->fetchAll();
-    $this->displayTable($rowtotal);
-  }
-public function displayTable($e){
-      
-      echo "<table border=2>";
-      echo "<tr><th>id</th><th>email</th><th>fname</th><th>lname</th><th>phone</th><th>birthday</th><th>gender</th><th>password</th></tr>";
-
-    foreach( $e as $row) {
-      echo "<tr>";
-      echo "<td>".$row['id']."</td>";
-      echo "<td>".$row['email']."</td>";
-      echo "<td>".$row['fname']."</td>";
-      echo "<td>".$row['lname']."</td>";
-      echo "<td>".$row['phone']."</td>";
-      echo "<td>".$row['birthday']."</td>";
-      echo "<td>".$row['gender']."</td>";
-      echo "<td>".$row['password']."</td>";
-      echo "</tr>";
-  }
-  
-  //print_r($e);
-
-echo "</table>";
-    
-    }
-}
-class accounts extends collection{
-}
-?>
+ }
+ ?>
